8330abe51923dad38027d2dcf9172d2bc95a8900
Tavo <tavo@Tavos-MacBook-Pro.local>
2025-09-21 12:19:57 -0700

    add custom handlers

--- main.go
+++ main.go
@@ -16,14 +16,22 @@ func main() {
 	mux := http.NewServeMux()
 	// When mux hasn't been assigned any handlers it simply returns 404
 
-	// 1. Create a file server handler.
-	// `http.Dir(".")` creates a file system rooted at the "." path.
+	// Create a file server handler.
 	fileServer := http.FileServer(http.Dir(filepathRoot))
 
-	// 2. Register the file server to handle requests for the root path.
-	// http.Handle("/", ...) tells the multiplexer to use the fileServer
-	// for any path that starts with `/`.
-	mux.Handle("/", fileServer)
+	// Register the file server to handle requests at /app/
+	mux.Handle("/app/", http.StripPrefix("/app/", fileServer))
+	// http.FileServer serves files relative to the directory
+	// you give it (http.Dir(filepathRoot)) and uses the full
+	// URL path to find files. Without stripping the "/app"
+	// prefix, it will look for files under filepathRoot/app/...
+	// which likely doesn’t exist.
+
+	// Readiness endpoints are used by external systems to check
+	// if our server is ready to receive traffic.
+	// The endpoint should be accessible at the /healthz path
+	// using any HTTP method.
+	mux.HandleFunc("/healthz", handlerReadiness)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -34,5 +42,14 @@ func main() {
 	log.Fatal(srv.ListenAndServe())
 }
 
+func handlerReadiness(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
 // Command to build and run the server:
+// go run .
+
+// Command to compile a binary and run it
 // go build -o out && ./out
