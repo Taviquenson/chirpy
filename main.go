6c350c3c0ee0adfad1466158e1031feae6e9fede
Tavo <tavo@Tavos-MacBook-Pro.local>
2025-09-22 10:47:08 -0700

    add stateful handlers & middleware

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32 // allows us to safely increment and
+	// read an integer value across multiple
+	// goroutines (HTTP requests)
+}
 
 func main() {
 	const filepathRoot = "."
@@ -17,10 +25,12 @@ func main() {
 	// When mux hasn't been assigned any handlers it simply returns 404
 
 	// Create a file server handler.
-	fileServer := http.FileServer(http.Dir(filepathRoot))
+	fileServerHandler := http.FileServer(http.Dir(filepathRoot))
+
+	apiCfg := apiConfig{} // fileserverHits is initialized to default value of 0
 
 	// Register the file server to handle requests at /app/
-	mux.Handle("/app/", http.StripPrefix("/app/", fileServer))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", fileServerHandler)))
 	// http.FileServer serves files relative to the directory
 	// you give it (http.Dir(filepathRoot)) and uses the full
 	// URL path to find files. Without stripping the "/app"
@@ -32,6 +42,8 @@ func main() {
 	// The endpoint should be accessible at the /healthz path
 	// using any HTTP method.
 	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerResetNumReq)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -42,10 +54,26 @@ func main() {
 	log.Fatal(srv.ListenAndServe())
 }
 
-func handlerReadiness(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, req *http.Request) {
+	// Set the Content-Type header to indicate the response body format.
+	w.Header().Set("Content-Type", "text/plain")
+	// Set the HTTP status code.
+	w.WriteHeader(http.StatusOK) // Or other status codes like http.StatusNotFound, http.StatusInternalServerError
+	// Write the response body.
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	// Wrap the given handler `next` in another nameless handler so that once
+	// middlewareMetricsInc() is called and registers this namesless handler,
+	// the count will increase and the `next` handle will execute evrytime the
+	// corresponding endpoint is reached because it is this nameless handler
+	// what will actually run on each request, or more specifically, its
+	// ServeHTTP method, which is the nameless function below
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
 
 // Command to build and run the server:
@@ -53,3 +81,6 @@ func handlerReadiness(w http.ResponseWriter, req *http.Request) {
 
 // Command to compile a binary and run it
 // go build -o out && ./out
+
+// Access server at:
+// localhost:8080
