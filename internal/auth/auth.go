585023531ab3a37b394fb971f2614c92606896ac
Taviquenson <tavo2070@gmail.com>
2025-09-30 10:43:20 -0700

    add auth with passwords

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,15 +1,13 @@
 package auth
 
 import (
-	"fmt"
-
 	"github.com/alexedwards/argon2id"
 )
 
 func HashPassword(password string) (string, error) {
 	hash, err := argon2id.CreateHash(password, argon2id.DefaultParams)
 	if err != nil {
-		return "", fmt.Errorf("couldn't hash the password: %w", err)
+		return "", err
 	}
 	return hash, nil
 }
@@ -17,7 +15,7 @@ func HashPassword(password string) (string, error) {
 func CheckPasswordHash(password, hash string) (bool, error) {
 	match, err := argon2id.ComparePasswordAndHash(password, hash)
 	if err != nil {
-		return false, fmt.Errorf("couldn't check password hash: %w", err)
+		return false, err
 	}
 	return match, nil
 }
---
+++ internal/auth/auth_test.go
@@ -0,0 +1,93 @@
+package auth
+
+import (
+	"testing"
+)
+
+// Boot.dev's example of how to properly test
+func TestCheckPasswordHash(t *testing.T) {
+	// First, we need to create some hashed passwords for testing
+	password1 := "correctPassword123!"
+	password2 := "anotherPassword456!"
+	hash1, _ := HashPassword(password1)
+	hash2, _ := HashPassword(password2)
+
+	tests := []struct {
+		name          string
+		password      string
+		hash          string
+		wantErr       bool
+		matchPassword bool
+	}{
+		{
+			name:          "Correct password",
+			password:      password1,
+			hash:          hash1,
+			wantErr:       false,
+			matchPassword: true,
+		},
+		{
+			name:          "Incorrect password",
+			password:      "wrongPassword",
+			hash:          hash1,
+			wantErr:       false,
+			matchPassword: false,
+		},
+		{
+			name:          "Password doesn't match different hash",
+			password:      password1,
+			hash:          hash2,
+			wantErr:       false,
+			matchPassword: false,
+		},
+		{
+			name:          "Empty password",
+			password:      "",
+			hash:          hash1,
+			wantErr:       false,
+			matchPassword: false,
+		},
+		{
+			name:          "Invalid hash",
+			password:      password1,
+			hash:          "invalidhash",
+			wantErr:       true,
+			matchPassword: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			match, err := CheckPasswordHash(tt.password, tt.hash)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CheckPasswordHash() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !tt.wantErr && match != tt.matchPassword {
+				t.Errorf("CheckPasswordHash() expects %v, got %v", tt.matchPassword, match)
+			}
+		})
+	}
+}
+
+// My tests
+func TestHashPassword(t *testing.T) {
+	hash, err := HashPassword("techie$4Good")
+	if err != nil {
+		t.Errorf(`%v`, err)
+	}
+	match, err := CheckPasswordHash("techie$4Good", hash)
+	if !match || err != nil {
+		t.Errorf(`%v`, err)
+	}
+}
+
+func TestHashPasswordDefault(t *testing.T) {
+	hash, err := HashPassword("unset")
+	if err != nil {
+		t.Errorf(`%v`, err)
+	}
+	match, err := CheckPasswordHash("unset", hash)
+	if !match || err != nil {
+		t.Errorf(`%v`, err)
+	}
+}
